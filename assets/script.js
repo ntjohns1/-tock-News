ca7b208c6dff4f189c423d8e273450bed0066488
ntjohns1 <nelsontjohns@gmail.com>
2021-03-13 13:27:03 -0500

    fetch request for currents API

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,20 @@
-// writing things 
-// and more
+var searchButton = document.querySelector("#search_button");
+var inputField = document.querySelector("#search_field")
+
+function newsAPI() {
+    var searchInput = inputField.value.trim();
+    var url = 'https://api.currentsapi.services/v1/latest-news?' + searchInput +
+    'language=us&' +
+    'apiKey=58zqUId_bFIYNSpOfAZh4cXhhgMJ1is-b48zhSmxe60fK5F5';
+    var req = new Request(url);
+    fetch(req)
+    .then(function(response) { 
+        return response.json();
+    })
+    .then(function(response) {
+    console.log(response);
+    })
+
+};
+
+searchButton.addEventListener("click", newsAPI);
