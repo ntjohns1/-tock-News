a9f97eda243d718037e8fbf55e1e33cc8399c538
sg0703 <samgates@Sams-MacBook-Pro.local>
2021-03-13 11:53:11 -0500

    created fetch request to test finnhub api

--- finance.js
+++ finance.js
@@ -1,1 +1,10 @@
 // Alpha Vantage API test
+
+// api key c162mdv48v6ootkka5hg
+var token = 'c162mdv48v6ootkka5hg';
+
+console.log(token);
+
+fetch('https://finnhub.io/docs/api/quote?symbol=AAPL&token='+token)
+  .then(response => response.json())
+  .then(data => console.log(data));
