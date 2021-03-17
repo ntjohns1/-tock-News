02ae1ca4eab49420902d2e476a457ac93130bdec
ntjohns1 <nelsontjohns@gmail.com>
2021-03-17 19:26:35 -0400

    merging with main branch

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var searchButton = document.querySelector("#search_button");
 var inputField = document.querySelector("#search_field")
 
@@ -17,4 +18,27 @@ function newsAPI() {
 
 };
 
+=======
+var newsAPIKey = '5978072a3f15452b82d992ec9921e3fc';
+var searchButton = document.querySelector("#button");
+var inputField = document.querySelector("#textbox");
+// cors proxy thing 
+var proxyUrl = "https://cors-anywhere.herokuapp.com/"
+
+function newsAPI() {
+    var searchInput = inputField.value.trim();
+    console.log(searchInput)
+    var url = 'https://api.currentsapi.services/v1/search?' + 'language=en&keywords=' + 'Apple' + '&apiKey=58zqUId_bFIYNSpOfAZh4cXhhgMJ1is-b48zhSmxe60fK5F5';
+    fetch(url)
+        .then(function (response) {
+          console.log(response);  
+          return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+
+};
+newsAPI();
+>>>>>>> Stashed changes
 searchButton.addEventListener("click", newsAPI);
